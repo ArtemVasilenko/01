6884bc243255b1a47a609af6e90332c7b5d6cf3f
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-05-13 20:33:05 +0300

    +

--- 01/ViewController.swift
+++ 01/ViewController.swift
@@ -1,20 +1,86 @@
-//
-//  ViewController.swift
-//  01
-//
-//  Created by Артем on 5/13/19.
-//  Copyright © 2019 Артем. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+// VFL
 
+
+
+class ViewController: UIViewController {
+    
+    let redView: UIView = {
+        let myView = UIView()
+        myView.backgroundColor = .red
+        myView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return myView
+    }()
+    
+    let blueView: UIView = {
+        let myView = UIView()
+        myView.backgroundColor = .blue
+        myView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return myView
+    }()
+    
+    
+    let greenView: UIView = {
+        let myView = UIView()
+        myView.backgroundColor = .green
+        myView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return myView
+    }()
+    
+    let blackView: UIView = {
+        let myView = UIView()
+        myView.backgroundColor = .black
+        myView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return myView
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.addSubview(redView)
+        self.view.addSubview(blueView)
+        self.view.addSubview(greenView)
+        self.view.addSubview(blackView)
+        
+        
+        let viewVFL = ["redView": redView, "blueView": blueView, "greenView": greenView, "blackView": blackView]
+        
+        let metrics = ["height": 100, "width": self.view.bounds.width/3, "top1": self.view.bounds.height/5, "top2": self.view.bounds.height/5 * 3]
+        
+        self.view.addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: "V:|-top1-[greenView(top1)]-top1-[redView(top1)]",
+            options: [], //расположение объекта внутри (центровка)
+            metrics: metrics,
+            views: viewVFL))
+        
+        self.view.addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: "V:|-top1-[blackView(top1)]-top1-[blueView(top1)]",
+            options: [], //расположение объекта внутри (центровка)
+            metrics: metrics,
+            views: viewVFL))
+        
+        
+        self.view.addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: "H:|-[greenView(width)]-50-[blackView(width)]-|",
+            options: [], //расположение объекта внутри (центровка)
+            metrics: metrics,
+            views: viewVFL))
+        
+        self.view.addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: "H:|-[redView(width)]-50-[blueView(width)]-|",
+            options: [], //расположение объекта внутри (центровка)
+            metrics: metrics,
+            views: viewVFL))
+        
     }
-
-
+    
 }
+
+
 
